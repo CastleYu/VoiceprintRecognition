02d8e29ca26463b5036bafdb692b44b28b887e4e
CastleYu <61407907+CastleYu@users.noreply.github.com>
2025-02-28 17:32:31 +0800

    [fix] 修复Milvus索引异常

--- dao/milvus_dao.py
+++ dao/milvus_dao.py
@@ -5,43 +5,52 @@ class MilvusClient:
     def __init__(self, host, port):
         connections.connect(alias='default', host=host, port=port)
         self.fields = [
-            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),  # 主键s
+            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
             FieldSchema(name="vec", dtype=DataType.FLOAT_VECTOR, dim=192)
         ]
+        self.index_params = {
+            "index_type": "IVF_FLAT",
+            "metric_type": "L2",
+            "params": {
+                "nlist": 128}
+        }
 
     def create_collection(self, table_name):
-        # 检查集合是否已经存在
         if utility.has_collection(table_name):
             collection = Collection(name=table_name)
+            # 确保已有集合存在索引，若不存在则创建
+            if not collection.has_index():
+                collection.create_index(field_name="vec", index_params=self.index_params)
             collection.load()
             return collection
 
-        # 定义集合的 schema
-
+        # 创建新集合并设置Schema
         schema = CollectionSchema(self.fields, description="audio collection")
-
-        # 创建集合
         collection = Collection(name=table_name, schema=schema)
+        # 创建索引
+        collection.create_index(field_name="vec", index_params=self.index_params)
+        # 加载集合到内存
         collection.load()
         return collection
 
     def insert(self, table_name, vectors):
-        # 确保集合已经创建
         collection = self.create_collection(table_name)
-        # 插入数据并返回 ID
-        ids = collection.insert([vectors]).primary_keys  # 获取 ID
+        # 插入数据并返回ID
+        ids = collection.insert([vectors]).primary_keys
         return ids
 
     def create_index(self, table_name):
         collection = Collection(name=table_name)
-        index_params = {"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}}
-        collection.create_index(field_name="vec", index_params=index_params)
+        collection.create_index(field_name="vec", index_params=self.index_params)
 
     def search(self, query_vectors, table_name, top_k=1):
         # 确保集合已经创建
         collection = self.create_collection(table_name)
         # 搜索
-        search_params = {"metric_type": "L2", "params": {"nprobe": 10}}
+        search_params = {
+            "metric_type": "L2",
+            "params": {
+                "nprobe": 10}}
         results = collection.search(data=[query_vectors], anns_field="vec", param=search_params, limit=top_k,
                                     output_fields=['vec'])
         return results
