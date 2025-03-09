64c82b1071420ec4ef057c9b6a803b6c60fda928
EricJay <404510957@qq.com>
2025-03-09 17:12:45 +0800

    文件名更改

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def do_search_action(action):
 @app.route('/load', methods=['PUT'])
 def load():
     # 硬编码 CSV 文件路径
-    CSV_FILE_PATH = r'P:\xiangmu\python\Voice\train_001.csv'
+    CSV_FILE_PATH = r'P:\xiangmu\python\Voice\train_new_001.csv'
 
     file_data = defaultdict(list)
 
@@ -291,7 +291,7 @@ def float_compare(a, b, precision=1e-9):
 @app.route('/recognizeAudioPrint', methods=['POST'])
 def recognizeAudioPrint():
     # 配置化 CSV 文件路径
-    CSV_FILE_PATH = r'P:\xiangmu\python\Voice\test_001.csv'
+    CSV_FILE_PATH = r'P:\xiangmu\python\Voice\test_new_001.csv'
 
     file_data = []
     # 检查请求中的文件是否有效
@@ -309,7 +309,7 @@ def recognizeAudioPrint():
         for row in csv_reader:
             file_data.append(row[0])  # 音频文件的本地位置
 
-    file = open(r'P:\xiangmu\python\Voice\result_001.csv', 'w', newline='')
+    file = open(r'P:\xiangmu\python\Voice\result_new_001.csv', 'w', newline='')
 
     try:
         for file_path in file_data:
